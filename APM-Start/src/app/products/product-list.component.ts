1c3d4ddc66665535dfb1f1a733b4d92b4ef10398
Diego Moreno <diegohmore@hotmail.com>
2020-08-30 20:28:50 -0500

    reacting to actions

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,10 +1,12 @@
-import { Component, ChangeDetectionStrategy  } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
-import { EMPTY, Observable } from 'rxjs';
+import { EMPTY, Observable, combineLatest, BehaviorSubject } from 'rxjs';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
 import { catchError, map } from 'rxjs/operators';
+import { ProductCategoryService } from '../product-categories/product-category.service';
+import { ProductCategory } from '../product-categories/product-category';
 
 @Component({
   templateUrl: './product-list.component.html',
@@ -14,9 +16,25 @@ import { catchError, map } from 'rxjs/operators';
 export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
-  categories;
 
-  products$: Observable<Product[]> = this.productService.productWithCategories$
+  private categorySelectedSubject = new BehaviorSubject<number>(0);
+  categorySelectedAction$: Observable<number> = this.categorySelectedSubject.asObservable();
+
+  products$: Observable<Product[]> = combineLatest([
+    this.productService.productWithCategories$,
+    this.categorySelectedAction$
+  ]).pipe(
+    map(([products, selectedCategotyId]) =>
+      products.filter(product =>
+        selectedCategotyId ? product.categoryId === selectedCategotyId : true
+      )),
+    catchError(err => {
+      this.errorMessage = err
+      return EMPTY;
+    })
+  );
+
+  categories$: Observable<ProductCategory[]> = this.productCategoryService.productCategories$
     .pipe(
       catchError(err => {
         this.errorMessage = err
@@ -24,13 +42,14 @@ export class ProductListComponent {
       })
     );
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+    private productCategoryService: ProductCategoryService) { }
 
   onAdd(): void {
     console.log('Not yet implemented');
   }
 
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+    this.categorySelectedSubject.next(+categoryId);
   }
 }
